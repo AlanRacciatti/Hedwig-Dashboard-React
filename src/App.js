10846f26baa0dfdd1721965624f9801322019b2b
AlanRacciatti <alanracciatti1220@gmail.com>
2021-10-27 19:44:56 -0300

    add sidebar y topbar2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-import MainCard from "./components/MainDataCards/main-data-card"
+import Sidebar from './components/Sidebar/Sidebar'
+import Topbar from './components/Topbar/Topbar'
+import './app.css';
 
 
 const App = () => {
   return(
-    <div>
-      <MainCard usuarios/>
-      <MainCard arbol/>
-      <MainCard/>
+    <div id="wrapper">
+      <Sidebar />
+      <div id="content-wrapper" className="d-flex flex-column">
+        <div id="content">
+          <Topbar />
+        </div>
+      </div>
     </div>
   )
 }
